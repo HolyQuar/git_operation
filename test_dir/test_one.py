ae017771cad79c06b985719ce8c7d0cb5702da58
HolyQuar <2365856524@qq.com>
2020-08-09 19:07:24 +0800

    modify master_py

--- test_dir/test_one.py
+++ test_dir/test_one.py
@@ -1,6 +1,6 @@
-if __name__=='__main__':
+if __name__ == '__main__':
     print('hello world~~~')
-    num=0
-    for _ in range(10):
-        num=+1
-    print('result is:',num)
+    num = 0
+    for i in range(10):
+        num +=1
+    print('result is:', num)
